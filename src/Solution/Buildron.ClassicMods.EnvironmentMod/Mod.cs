dbfe505723ff1f9a21f491d61f8595ff2535c8b9
Diego Giacomelli <giacomelli@gmail.com>
2016-07-31 19:08:48 -0300

    EnvironmentMod is ready to go.

---
+++ src/Solution/Buildron.ClassicMods.EnvironmentMod/Mod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Buildron.Domain.Mods;
+using UnityEngine;
+
+namespace Buildron.ClassicMods.EnvironmentMod
+{
+	public class Mod : IMod
+	{
+		public void Initialize (IModContext context)
+		{
+			context.CreateGameObjectFromPrefab ("EnvironmentPrefab");
+
+			var cam = context.Camera.MainCamera;
+			cam.clearFlags = CameraClearFlags.SolidColor;
+			cam.backgroundColor = Color.black;
+		}
+	}
+}
+
