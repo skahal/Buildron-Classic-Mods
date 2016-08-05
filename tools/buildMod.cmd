b87fe2f7017725676ffa575a40be8e8a84cc4dac
Diego Giacomelli <giacomelli@gmail.com>
2016-08-04 21:33:49 -0300

    Improved build script.

--- tools/buildMod.cmd
+++ tools/buildMod.cmd
@@ -1,3 +1,5 @@
 @echo Building %1
-@"C:\Program Files\Unity\Editor\Unity.exe" -projectPath %cd%\..\src\Buildron.ClassicMods.%1 -quit -batchmode -executeMethod ModBuilder.BuildFromCommandLine %cd%\..\build\Mods StandaloneWindows
+@"C:\Program Files\Unity\Editor\Unity.exe" -projectPath %cd%\..\src\Buildron.ClassicMods.%1 -quit -batchmode -executeMethod ModBuilder.BuildFromCommandLine %cd%\..\..\Buildron\build\Mods StandaloneWindows
 @echo %1 done.
+pause
+exit
