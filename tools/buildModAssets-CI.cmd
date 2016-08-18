bdcb32c7fa3df462ed6f69c0ea8a22d7b3ad67f3
Diego Giacomelli <giacomelli@gmail.com>
2016-08-18 18:51:29 -0300

    Testing AppVeyor.

--- tools/buildModAssets-CI.cmd
+++ tools/buildModAssets-CI.cmd
@@ -1,3 +1,3 @@
 @echo Building %1 mod assets...
 echo on
-@"C:\Program Files\Unity\Editor\Unity.exe" -projectPath  C:\projects\buildron-classic-mods\src\Buildron.ClassicMods.%1 -username "%2" -password "%3" -serial %4 -quit -batchmode -executeMethod ModBuilder.BuildFromCommandLine  C:\projects\buildron-classic-mods\build StandaloneWindows
+@"C:\Program Files\Unity\Editor\Unity.exe" -projectPath  C:\projects\buildron-classic-mods\src\Buildron.ClassicMods.%1 -nographics -quit -batchmode -executeMethod ModBuilder.BuildFromCommandLine  C:\projects\buildron-classic-mods\build StandaloneWindows
